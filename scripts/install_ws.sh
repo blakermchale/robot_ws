e597e35af307122ff3302d22993ec000b9bd8f0e
Blake McHale <mchale.blake@gmail.com>
2021-06-30 23:40:13 -0400

    Accept all installations in install script

--- scripts/install_ws.sh
+++ scripts/install_ws.sh
@@ -1,5 +1,5 @@
-sudo apt install ros-foxy-mavlink ros-foxy-geographic-msgs libgeographic-dev ros-foxy-eigen-stl-containers ros-foxy-diagnostic-updater ros-foxy-behaviortree-cpp-v3 ros-foxy-navigation2
-sudo apt install libignition-common3-graphics-dev
-sudo apt install  python3-venv python-is-python3 python3-colcon-common-extensions
+sudo apt install ros-foxy-mavlink ros-foxy-geographic-msgs libgeographic-dev ros-foxy-eigen-stl-containers ros-foxy-diagnostic-updater ros-foxy-behaviortree-cpp-v3 ros-foxy-navigation2 -y
+sudo apt install libignition-common3-graphics-dev -y
+sudo apt install  python3-venv python-is-python3 python3-colcon-common-extensions -y
 # Groot depndencies
-sudo apt install qtbase5-dev libqt5svg5-dev libzmq3-dev libdw-dev
+sudo apt install qtbase5-dev libqt5svg5-dev libzmq3-dev libdw-dev -y
