263d6dc03dcadb7a5e3efc769fac62129929c34c
Blake McHale <mchale.blake@gmail.com>
2021-07-02 13:59:48 -0400

    Update install

--- scripts/install_ws.sh
+++ scripts/install_ws.sh
@@ -1,4 +1,4 @@
-sudo apt install ros-galactic-mavlink ros-galactic-geographic-msgs libgeographic-dev ros-galactic-eigen-stl-containers ros-galactic-diagnostic-updater ros-galactic-behaviortree-cpp-v3 ros-galactic-test-msgs ros-foxy-galactic-bondcpp ros-galactic-ompl ros-galactic-xacro -y
+sudo apt install ros-galactic-mavlink ros-galactic-geographic-msgs libgeographic-dev ros-galactic-eigen-stl-containers ros-galactic-diagnostic-updater ros-galactic-behaviortree-cpp-v3 ros-galactic-test-msgs ros-galactic-bondcpp ros-galactic-ompl ros-galactic-xacro ros-galactic-gazebo-ros-pkgs -y
 sudo apt install libignition-common3-graphics-dev -y
 sudo apt install  python3-venv python-is-python3 python3-colcon-common-extensions -y
 # Groot depndencies
